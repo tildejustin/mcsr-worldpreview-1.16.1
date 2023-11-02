75440cdbfdab211c52094127a5cc51b4083b1f22
contaria <kingcontaria@gmail.com>
2023-11-02 16:05:29 +0100

    cleanup AbstractClientPlayerEntityMixin

--- src/main/java/me/voidxwalker/worldpreview/mixin/client/AbstractClientPlayerEntityMixin.java
+++ src/main/java/me/voidxwalker/worldpreview/mixin/client/AbstractClientPlayerEntityMixin.java
@@ -1,24 +1,16 @@
 package me.voidxwalker.worldpreview.mixin.client;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.mojang.authlib.GameProfile;
 import me.voidxwalker.worldpreview.WorldPreview;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.PlayerListEntry;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(AbstractClientPlayerEntity.class)
-public abstract class AbstractClientPlayerEntityMixin extends PlayerEntity {
-
-    public AbstractClientPlayerEntityMixin(World world, BlockPos blockPos, GameProfile gameProfile) {
-        super(world, blockPos, gameProfile);
-    }
+public abstract class AbstractClientPlayerEntityMixin {
 
     @ModifyExpressionValue(method = "*", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;getNetworkHandler()Lnet/minecraft/client/network/ClientPlayNetworkHandler;"))
     private ClientPlayNetworkHandler modifyNetworkHandler(ClientPlayNetworkHandler networkHandler) {
