e120469bd814aadabe9a5549c03c7bc6dd8ab5d0
contaria <kingcontaria@gmail.com>
2023-11-02 21:45:55 +0100

    fix concurrency issues with rendering entities

--- src/main/java/me/voidxwalker/worldpreview/mixin/client/render/WorldRendererMixin.java
+++ src/main/java/me/voidxwalker/worldpreview/mixin/client/render/WorldRendererMixin.java
@@ -1,5 +1,6 @@
 package me.voidxwalker.worldpreview.mixin.client.render;
 
+import com.google.common.collect.ImmutableSet;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import me.voidxwalker.worldpreview.WorldPreview;
@@ -60,6 +61,12 @@ public abstract class WorldRendererMixin {
             return null;
         }
         return entity;
+    }
+
+    // fixes threading concurrency issues with adding entities to the world while rendering
+    @ModifyExpressionValue(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;getEntities()Ljava/lang/Iterable;"))
+    private Iterable<Entity> fixEntityIteration(Iterable<Entity> entities) {
+        return ImmutableSet.copyOf(entities);
     }
 
     @Unique
