31ce19b6e7606b6ace3ffb99b9c9c463a0a880c4
contaria <kingcontaria@gmail.com>
2023-12-04 23:56:42 +0100

    remove fastreset logic from worldpreview fastreset now automatically uses fastreset on wp resets

--- src/main/java/me/voidxwalker/worldpreview/mixin/server/MinecraftServerMixin.java
+++ src/main/java/me/voidxwalker/worldpreview/mixin/server/MinecraftServerMixin.java
@@ -2,11 +2,11 @@ package me.voidxwalker.worldpreview.mixin.server;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import me.voidxwalker.worldpreview.WorldPreview;
-import me.voidxwalker.worldpreview.interfaces.FastCloseable;
 import me.voidxwalker.worldpreview.interfaces.WPMinecraftServer;
 import me.voidxwalker.worldpreview.mixin.access.SpawnLocatingAccessor;
 import net.minecraft.client.MinecraftClient;
@@ -17,9 +17,12 @@ import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.PlayerManager;
+import net.minecraft.server.WorldGenerationProgressListener;
+import net.minecraft.server.world.ChunkTicketType;
+import net.minecraft.server.world.ServerChunkManager;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.GameMode;
 import org.jetbrains.annotations.Nullable;
@@ -31,7 +34,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.io.IOException;
 import java.util.Random;
 
 @Mixin(MinecraftServer.class)
@@ -102,9 +104,17 @@ public abstract class MinecraftServerMixin implements WPMinecraftServer {
         return serverWorld;
     }
 
+    @WrapOperation(method = "prepareStartRegion", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerChunkManager;addTicket(Lnet/minecraft/server/world/ChunkTicketType;Lnet/minecraft/util/math/ChunkPos;ILjava/lang/Object;)V"))
+    private void captureChunkTicketInformation(ServerChunkManager chunkManager, ChunkTicketType<Object> ticketType, ChunkPos pos, int radius, Object argument, Operation<Void> original, @Share("removeTicket") LocalRef<Runnable> removeTicket) {
+        removeTicket.set(() -> chunkManager.removeTicket(ticketType, pos, radius, argument));
+        original.call(chunkManager, ticketType, pos, radius, argument);
+    }
+
     @Inject(method = "prepareStartRegion", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerChunkManager;getTotalChunksLoadedCount()I", shift = At.Shift.AFTER), cancellable = true)
-    private void killWorldGen(CallbackInfo ci) {
+    private void killWorldGen(WorldGenerationProgressListener worldGenerationProgressListener, CallbackInfo ci, @Share("removeTicket") LocalRef<Runnable> removeTicket) {
         if (this.killed) {
+            removeTicket.get().run();
+            worldGenerationProgressListener.stop();
             ci.cancel();
         }
     }
@@ -118,28 +128,6 @@ public abstract class MinecraftServerMixin implements WPMinecraftServer {
     private synchronized boolean killServer(boolean original) {
         this.tooLateToKill = true;
         return original && !this.killed;
-    }
-
-    @ModifyExpressionValue(method = "shutdown", at = @At(value = "FIELD", target = "Lnet/minecraft/server/MinecraftServer;playerManager:Lnet/minecraft/server/PlayerManager;", ordinal = 0))
-    private PlayerManager killPlayerSave(PlayerManager playerManager) {
-        if (this.killed) {
-            return null;
-        }
-        return playerManager;
-    }
-
-    @WrapOperation(method = "shutdown", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;close()V"))
-    private void killServerWorldClose(ServerWorld serverWorld, Operation<Void> original) throws IOException {
-        if (this.killed) {
-            ((FastCloseable) serverWorld.getChunkManager().threadedAnvilChunkStorage).worldpreview$fastClose();
-            return;
-        }
-        original.call(serverWorld);
-    }
-
-    @WrapWithCondition(method = "shutdown", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;save(ZZZ)Z"))
-    private boolean killSave(MinecraftServer server, boolean bl, boolean bl2, boolean bl3) {
-        return !this.killed;
     }
 
     /**
@@ -163,7 +151,7 @@ public abstract class MinecraftServerMixin implements WPMinecraftServer {
             long l = (long)(i * 2 + 1);
             long m = l * l;
             int k = m > 2147483647L ? Integer.MAX_VALUE : (int)m;
-            int n = k <= 16 ? k - 1 : 17;;
+            int n = k <= 16 ? k - 1 : 17;
             int o = (new Random()).nextInt(k);
 
             for (int p = 0; p < k; ++p) {
