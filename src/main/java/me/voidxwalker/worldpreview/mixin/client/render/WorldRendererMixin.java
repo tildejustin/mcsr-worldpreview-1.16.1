01fe101a9abe888d707028781b1f56bf155df08e
contaria <kingcontaria@gmail.com>
2023-12-21 14:52:34 +0100

    remove fixWorldPreviewChunkRebuilding mixins they became redundant at some point apparently, thats the power of doing things the way they were intended to be done

--- src/main/java/me/voidxwalker/worldpreview/mixin/client/render/WorldRendererMixin.java
+++ src/main/java/me/voidxwalker/worldpreview/mixin/client/render/WorldRendererMixin.java
@@ -1,11 +1,9 @@
 package me.voidxwalker.worldpreview.mixin.client.render;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import me.voidxwalker.worldpreview.WorldPreview;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.render.chunk.ChunkBuilder;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import org.objectweb.asm.Opcodes;
@@ -13,22 +11,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
-import java.util.Collection;
-import java.util.Set;
-
-// Priority is set to 1500 so the fixWorldPreviewChunkRebuilding mixins don't get applied if sodiums overwrite is present
-@Mixin(value = WorldRenderer.class, priority = 1500)
+@Mixin(WorldRenderer.class)
 public abstract class WorldRendererMixin {
-
-    @WrapWithCondition(method = "setupTerrain", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/chunk/ChunkBuilder$BuiltChunk;cancelRebuild()V"), require = 0)
-    private boolean fixWorldPreviewChunkRebuilding(ChunkBuilder.BuiltChunk builtChunk) {
-        return !this.isWorldPreview();
-    }
-
-    @WrapWithCondition(method = "setupTerrain", at = @At(value = "INVOKE", target = "Ljava/util/Set;addAll(Ljava/util/Collection;)Z"), require = 0)
-    private boolean fixWorldPreviewChunkRebuilding(Set<ChunkBuilder.BuiltChunk> set, Collection<ChunkBuilder.BuiltChunk> collection) {
-        return !this.isWorldPreview();
-    }
 
     @ModifyExpressionValue(method = "*", at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient;world:Lnet/minecraft/client/world/ClientWorld;", opcode = Opcodes.GETFIELD))
     private ClientWorld modifyWorld(ClientWorld world) {
