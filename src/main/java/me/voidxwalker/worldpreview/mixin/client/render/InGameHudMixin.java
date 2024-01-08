55e09a510a3bc9c1f8de73e9d75a646dfedf48ef
contaria <kingcontaria@gmail.com>
2024-01-08 03:35:00 +0100

    fix heldItemTooltipFade when itemstack is the same

--- src/main/java/me/voidxwalker/worldpreview/mixin/client/render/InGameHudMixin.java
+++ src/main/java/me/voidxwalker/worldpreview/mixin/client/render/InGameHudMixin.java
@@ -6,10 +6,14 @@ import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.item.ItemStack;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
+
+    @Shadow
+    private ItemStack currentStack;
 
     @ModifyExpressionValue(method = "renderHeldItemTooltip", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/hud/InGameHud;currentStack:Lnet/minecraft/item/ItemStack;", opcode = Opcodes.GETFIELD))
     private ItemStack modifyCurrentStack(ItemStack currentStack) {
@@ -22,6 +26,10 @@ public abstract class InGameHudMixin {
     @ModifyExpressionValue(method = "renderHeldItemTooltip", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/hud/InGameHud;heldItemTooltipFade:I", opcode = Opcodes.GETFIELD))
     private int modifyHeldItemTooltipFade(int heldItemTooltipFade) {
         if (WorldPreview.renderingPreview) {
+            // see InGameHud#tick, the check needs to be performed because vanilla doesn't reset InGameHud#currentStack when changing worlds
+            if (WorldPreview.player.getMainHandStack().getItem() == this.currentStack.getItem() && WorldPreview.player.getMainHandStack().getName().equals(this.currentStack.getName())) {
+                return heldItemTooltipFade;
+            }
             return 40;
         }
         return heldItemTooltipFade;
