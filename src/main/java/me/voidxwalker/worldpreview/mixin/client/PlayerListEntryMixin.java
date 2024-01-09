8726257689b2d83f8b9d5e940adb44fc9a4eca68
contaria <kingcontaria@gmail.com>
2024-01-09 10:44:30 +0100

    fix skins not working on preview since skins get loaded in a MinecraftClient#execute, they aren't loaded while the preview is actually rendering, so instead of modifying the fields of usage we just modify the map itself, which is cleaner anyway

--- src/main/java/me/voidxwalker/worldpreview/mixin/client/PlayerListEntryMixin.java
+++ src/main/java/me/voidxwalker/worldpreview/mixin/client/PlayerListEntryMixin.java
@@ -1,27 +1,26 @@
 package me.voidxwalker.worldpreview.mixin.client;
 
+import com.google.common.collect.Maps;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import me.voidxwalker.worldpreview.WorldPreview;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.util.Identifier;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.EnumMap;
-import java.util.Map;
 
 @Mixin(PlayerListEntry.class)
 public abstract class PlayerListEntryMixin {
 
     @Unique
-    private static final Map<MinecraftProfileTexture.Type, Identifier> TEXTURES_CACHE = new EnumMap<>(MinecraftProfileTexture.Type.class);
+    private static final EnumMap<MinecraftProfileTexture.Type, Identifier> TEXTURES_CACHE = Maps.newEnumMap(MinecraftProfileTexture.Type.class);
 
     // uses the same textures map for all previews, without this cache skins only get loaded at the end of the preview
-    @ModifyExpressionValue(method = "*", at = @At(value = "FIELD", target = "Lnet/minecraft/client/network/PlayerListEntry;textures:Ljava/util/Map;", opcode = Opcodes.GETFIELD))
-    private Map<MinecraftProfileTexture.Type, Identifier> modifyTextures(Map<MinecraftProfileTexture.Type, Identifier> textures) {
+    @ModifyExpressionValue(method = "<init>", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Maps;newEnumMap(Ljava/lang/Class;)Ljava/util/EnumMap;"))
+    private EnumMap<MinecraftProfileTexture.Type, Identifier> modifyTextures(EnumMap<MinecraftProfileTexture.Type, Identifier> textures) {
         if (WorldPreview.renderingPreview) {
             return TEXTURES_CACHE;
         }
