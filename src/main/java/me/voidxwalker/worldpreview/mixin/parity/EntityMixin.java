9baed50dc8d33ab033a23d1a933acd869dff4ba0
contaria <kingcontaria@gmail.com>
2024-07-14 18:54:02 +0200

    fix: dont increment entity id counter on worldpreview entities

---
+++ src/main/java/me/voidxwalker/worldpreview/mixin/parity/EntityMixin.java
@@ -0,0 +1,25 @@
+package me.voidxwalker.worldpreview.mixin.parity;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import me.voidxwalker.worldpreview.WorldPreview;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Mixin(Entity.class)
+public abstract class EntityMixin {
+
+    @WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Ljava/util/concurrent/atomic/AtomicInteger;incrementAndGet()I"))
+    private int doNotIncrementEntityIdOnPreview(AtomicInteger MAX_ENTITY_ID, Operation<Integer> original) {
+        // for vanilla parity (where these entities wouldn't be created) we skip incrementing MAX_ENTITY_ID
+        // the entity id will be set in the ClientPlayNetworkHandler anyway
+        if (MinecraftClient.getInstance().isOnThread() && WorldPreview.renderingPreview) {
+            return -1;
+        }
+        return original.call(MAX_ENTITY_ID);
+    }
+}
