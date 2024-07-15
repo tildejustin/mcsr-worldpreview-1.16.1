1c96119fb603ee1623c09affea8c8f0289c18357
contaria <kingcontaria@gmail.com>
2024-07-15 17:32:36 +0200

    change: use WrapMethod to synchronize camera updates this means other mods like seedqueue dont have to synchronize their usages anymore, improving mod compat

--- src/main/java/me/voidxwalker/worldpreview/mixin/client/CameraMixin.java
+++ src/main/java/me/voidxwalker/worldpreview/mixin/client/CameraMixin.java
@@ -1,8 +1,12 @@
 package me.voidxwalker.worldpreview.mixin.client;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import me.voidxwalker.worldpreview.WorldPreview;
 import net.minecraft.client.render.Camera;
+import net.minecraft.entity.Entity;
+import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,6 +20,11 @@ public abstract class CameraMixin {
             return WorldPreview.player.getStandingEyeHeight();
         }
         return original;
+    }
+
+    @WrapMethod(method = "update")
+    public synchronized void synchronizeCameraUpdate(BlockView area, Entity focusedEntity, boolean thirdPerson, boolean inverseView, float tickDelta, Operation<Void> original) {
+        original.call(area, focusedEntity, thirdPerson, inverseView, tickDelta);
     }
 
     @Unique
